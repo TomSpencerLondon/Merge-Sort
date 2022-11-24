5c1621071230a47decef56a104d9d1935bc94cc0
Tom Spencer <thomas.spencer@codurance.com>
2022-11-24 12:30:35 +0000

    Delete special case for two

--- src/test/java/com/tomspencerlondon/SortTest.java
+++ src/test/java/com/tomspencerlondon/SortTest.java
@@ -16,46 +16,29 @@ public class SortTest {
     assertThat(sort(List.of(1, 2))).isSorted();
     assertThat(sort(List.of(2, 1))).isSorted();
 
-    assertThat(sort(List.of(1, 3, 2)))
-        .isEqualTo(List.of(1, 2, 3));
-    assertThat(sort(List.of(2, 1, 3)))
-        .isEqualTo(List.of(1, 2, 3));
-    assertThat(sort(List.of(2, 1, 3)))
-        .isEqualTo(List.of(1, 2, 3));
-    assertThat(sort(List.of(3, 2, 1)))
-        .isEqualTo(List.of(1, 2, 3));
-    assertThat(sort(List.of(1, 2, 3, 4)))
-        .isEqualTo(List.of(1, 2, 3, 4));
-    assertThat(sort(List.of(2, 1, 3, 4)))
-        .isEqualTo(List.of(1, 2, 3, 4));
-    assertThat(sort(List.of(4, 3, 2, 1)))
-        .isEqualTo(List.of(1, 2, 3, 4));
-    assertThat(sort(List.of(1, 3, 1, 2)))
-        .isEqualTo(List.of(1, 1, 2, 3));
+    assertThat(sort(List.of(1, 3, 2))).isEqualTo(List.of(1, 2, 3));
+    assertThat(sort(List.of(2, 1, 3))).isEqualTo(List.of(1, 2, 3));
+    assertThat(sort(List.of(2, 1, 3))).isEqualTo(List.of(1, 2, 3));
+    assertThat(sort(List.of(3, 2, 1))).isEqualTo(List.of(1, 2, 3));
+    assertThat(sort(List.of(1, 2, 3, 4))).isEqualTo(List.of(1, 2, 3, 4));
+    assertThat(sort(List.of(2, 1, 3, 4))).isEqualTo(List.of(1, 2, 3, 4));
+    assertThat(sort(List.of(4, 3, 2, 1))).isEqualTo(List.of(1, 2, 3, 4));
+    assertThat(sort(List.of(1, 3, 1, 2))).isEqualTo(List.of(1, 1, 2, 3));
   }
 
   private List<Integer> sort(List<Integer> list) {
     if (list.size() <= 1) {
       return list;
-    } else if (list.size() == 2) {
-      int first = list.get(0);
-      int second = list.get(1);
-      if (first > second) {
-        return List.of(second, first);
-      } else {
-        return List.of(first, second);
-      }
-    } else {
-      int middle = list.get(0);
-      List<Integer> middles = list.stream().filter(x -> x == middle).toList();
-      List<Integer> lessers = list.stream().filter(x -> x < middle).toList();
-      List<Integer> greaters = list.stream().filter(x -> x > middle).toList();
-
-      List<Integer> result = new ArrayList<>();
-      result.addAll(sort(lessers));
-      result.addAll(middles);
-      result.addAll(sort(greaters));
-      return result;
     }
+    int middle = list.get(0);
+    List<Integer> middles = list.stream().filter(x -> x == middle).toList();
+    List<Integer> lessers = list.stream().filter(x -> x < middle).toList();
+    List<Integer> greaters = list.stream().filter(x -> x > middle).toList();
+
+    List<Integer> result = new ArrayList<>();
+    result.addAll(sort(lessers));
+    result.addAll(middles);
+    result.addAll(sort(greaters));
+    return result;
   }
 }
