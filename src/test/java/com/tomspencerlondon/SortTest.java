22c72b49ec662de7ade3186a23f198475ecfa33f
Tom Spencer <thomas.spencer@codurance.com>
2022-11-24 11:38:26 +0000

    add first empty list test

---
+++ src/test/java/com/tomspencerlondon/SortTest.java
@@ -0,0 +1,19 @@
+package com.tomspencerlondon;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+
+public class SortTest {
+
+  @Test
+  void testSort() {
+    assertThat(sort(asList())).isSorted();
+  }
+
+  private List<Integer> sort(List<Integer> list) {
+    return asList();
+  }
+}
